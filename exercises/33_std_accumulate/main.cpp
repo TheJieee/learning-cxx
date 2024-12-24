227bfdb1bf4642131c2ce6e6ec8ec0a4671cc890
TheJieee <patrick.jieee@gmail.com>
2024-12-24 21:53:05 +0800

    finished

--- exercises/33_std_accumulate/main.cpp
+++ exercises/33_std_accumulate/main.cpp
@@ -12,6 +12,7 @@ int main(int argc, char **argv) {
     //       - 连续存储；
     //       的张量占用的字节数
     // int size =
+    int size = sizeof(float) * std::accumulate(shape, shape + 4, 1, [](int a, int b) ->int {return a * b;});
     ASSERT(size == 602112, "4x1x3x224x224 = 602112");
     return 0;
 }
